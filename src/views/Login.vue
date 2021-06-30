993accec45b8bb2ea6766224b470a7a0bff0d09e
JinsonLeung <1053438108@qq.com>
2021-06-30 15:00:57 +0800

    add Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script lang='ts'>
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  setup() {
+    const refData = ref(0);
+    return {
+      refData
+    };
+  }
+});
+</script>
+<style scoped>
+</style>
